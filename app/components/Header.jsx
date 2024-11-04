c634924f8b0a5950a48467e20bad40c69b2f4243
unknown <thobhi2@gmail.com>
2024-11-04 19:34:49 +0100

    implimented header responsiveness for both mobile and desktop

---
+++ app/components/Header.jsx
@@ -0,0 +1,88 @@
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import { Search, Menu, X } from "lucide-react";
+
+function Header() {
+  const [isMenuOPen, setIsMenuOpen] = useState(false);
+
+  return (
+    <nav className="fixed font-fira top-0 left-0 right-0 bg-gradient-to-br from-rose-50 to-white/80 backdrop-blur-sm z-50 px-4 md:px-8 lg:px-16">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex justify-between items-center h-16">
+          <Link href="/" className="text-shadow-default text-2xl font-semibold">
+            Synny Blotto
+          </Link>
+
+          <div className="hidden md:flex items-center space-x-10">
+            <Link
+              href="/shop"
+              className="text-gray-700 hover:text-red-500 font-medium"
+            >
+              Shop
+            </Link>
+            <Link
+              href="/about"
+              className="text-gray-700 hover:text-red-500 font-medium"
+            >
+              About
+            </Link>
+            <Link
+              href="/blog"
+              className="text-gray-700 hover:text-red-500 font-medium"
+            >
+              Blog
+            </Link>
+            <Link
+              href="/faq"
+              className="text-gray-700 hover:text-red-500 font-medium"
+            >
+              FAQ
+            </Link>
+            <button className="bg-red-500 p-1 rounded-md text-white">
+              <Search size={20} />
+            </button>
+          </div>
+          <button
+            className="md:hidden p-2"
+            onClick={() => setIsMenuOpen(!isMenuOPen)}
+          >
+            {isMenuOPen ? <X size={24} /> : <Menu size={24} />}
+          </button>
+        </div>
+        {isMenuOPen && (
+          <div className="md:hidden py-4">
+            <div className="flex flex-col space-y-4">
+              <Link
+                href="/shop"
+                className="text-gray-700 hover:text-gray-900 font-bold"
+              >
+                Shop
+              </Link>
+              <Link
+                href="/about"
+                className="text-gray-700 hover:text-gray-900 font-bold"
+              >
+                About
+              </Link>
+              <Link
+                href="/blog"
+                className="text-gray-700 hover:text-gray-900 font-bold"
+              >
+                Blog
+              </Link>
+              <Link
+                href="/faq"
+                className="text-gray-700 hover:text-gray-900 font-bold"
+              >
+                FAQ
+              </Link>
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,11 +9,41 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-fira-sans-condensed)", ...fontFamily.sans],
+        fira: ["var(--font-fira-sans-condensed)"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      textShadow: {
+        default: "2px 2px 0 rgba(0, 0, 0, 0.5)",
+        md: "3px 3px 0 rgba(0, 0, 0, 0.5)",
+        lg: "4px 4px 0 rgba(0, 0, 0, 0.5)",
+      },
     },
   },
-  plugins: [],
+  variants: {
+    extend: {
+      textShadow: ["responsive", "hover", "focus"],
+    },
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      const textShadowUtilities = {
+        ".text-shadow-default": {
+          textShadow: "1px 3px 4px rgba(0, 0, 0, 0.2)",
+        },
+
+        ".text-shadow-md": {
+          textShadow: "3px 3px 0 rgba(0, 0, 0, 0.5)",
+        },
+        ".text-shadow-lg": {
+          textShadow: "4px 4px 0 rgba(0, 0, 0, 0.5)",
+        },
+      };
+      addUtilities(textShadowUtilities, ["responsive", "hover", "focus"]);
+    },
+  ],
 };
